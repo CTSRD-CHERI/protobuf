e748ad88e77b7b803f3a5a1f6465287581501123
Antoine Pitrou <antoine@python.org>
2022-07-19 16:40:28 +0200

    Fix #9947: make the ABI identical between debug and non-debug builds
    (cherry picked from commit 0574167d92a232cb8f5a9107aabda0aefbc39e8b)

--- src/google/protobuf/metadata_lite.h
+++ src/google/protobuf/metadata_lite.h
@@ -70,15 +70,19 @@ class InternalMetadata {
     GOOGLE_DCHECK(!is_message_owned || arena != nullptr);
   }
 
-#if defined(NDEBUG) || defined(_MSC_VER)
+  // To keep the ABI identical between debug and non-debug builds,
+  // the destructor is always defined here even though it may delegate
+  // to a non-inline private method.
+  // (see https://github.com/protocolbuffers/protobuf/issues/9947)
   ~InternalMetadata() {
+#if defined(NDEBUG) || defined(_MSC_VER)
     if (HasMessageOwnedArenaTag()) {
       delete reinterpret_cast<Arena*>(ptr_ - kMessageOwnedArenaTagMask);
     }
-  }
 #else
-  ~InternalMetadata();
+    CheckedDestruct();
 #endif
+  }
 
   template <typename T>
   void Delete() {
@@ -269,6 +273,9 @@ class InternalMetadata {
   PROTOBUF_NOINLINE void DoSwap(T* other) {
     mutable_unknown_fields<T>()->Swap(other);
   }
+
+  // Private helper with debug checks for ~InternalMetadata()
+  void CheckedDestruct();
 };
 
 // String Template specializations.
