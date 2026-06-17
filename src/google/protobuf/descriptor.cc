ee28194340d297f27973a418b6cd885995979392
Graeme Jenkinson <graeme@capabilitieslimited.co.uk>
2026-06-17 09:15:24 +0100

    Ensure that new_locs remains valid
    After the move, new_locs must be valid (and can be destroyed) but in an
    unspecified state. In this instance (as the arena values are the same,
    both null) the Move constructor for SourceCodeInfo_Location swaps the
    values. This results in new_locs possessing the offset value of loc.
    This offset is invalid as it indexes outside of the
    SourceCodeInfo_Location. This results in a capability bounds fault in
    the destructor when built in the Debug configurationi (as a result of
    cheecks that call GetArena()).

--- src/google/protobuf/descriptor.cc
+++ src/google/protobuf/descriptor.cc
@@ -9887,7 +9887,8 @@ void DescriptorBuilder::OptionInterpreter::UpdateSourceCodeInfo(
   // that need to be changed, there is zero copy overhead).
 
   RepeatedPtrField<SourceCodeInfo_Location>* locs = info->mutable_location();
-  RepeatedPtrField<SourceCodeInfo_Location> new_locs;
+  SourceCodeInfo new_src_code_info;
+  RepeatedPtrField<SourceCodeInfo_Location>* new_locs = new_src_code_info.mutable_location();
   bool copying = false;
 
   std::vector<int> pathv;
@@ -9927,7 +9928,7 @@ void DescriptorBuilder::OptionInterpreter::UpdateSourceCodeInfo(
     if (entry == interpreted_paths_.end()) {
       // not a match
       if (copying) {
-        *new_locs.Add() = *loc;
+        *new_locs->Add() = *loc;
       }
       continue;
     }
@@ -9937,16 +9938,16 @@ void DescriptorBuilder::OptionInterpreter::UpdateSourceCodeInfo(
     if (!copying) {
       // initialize the copy we are building
       copying = true;
-      new_locs.Reserve(locs->size());
+      new_locs->Reserve(locs->size());
       for (RepeatedPtrField<SourceCodeInfo_Location>::iterator it =
                locs->begin();
            it != loc; it++) {
-        *new_locs.Add() = *it;
+        *new_locs->Add() = *it;
       }
     }
 
     // add replacement and update its path
-    SourceCodeInfo_Location* replacement = new_locs.Add();
+    SourceCodeInfo_Location* replacement = new_locs->Add();
     *replacement = *loc;
     replacement->clear_path();
     for (std::vector<int>::iterator rit = entry->second.begin();
@@ -9957,7 +9958,7 @@ void DescriptorBuilder::OptionInterpreter::UpdateSourceCodeInfo(
 
   // if we made a changed copy, put it in place
   if (copying) {
-    *locs = std::move(new_locs);
+    *locs = std::move(*new_locs);
   }
 }
 
